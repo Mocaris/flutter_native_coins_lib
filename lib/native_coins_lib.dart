9cd23b363d054d38d3ac2fbc0c39f44522b6fcec
mocaris <imocaris@outlook.com>
2021-12-10 14:42:17 +0800

    update

--- lib/native_coins_lib.dart
+++ lib/native_coins_lib.dart
@@ -2,52 +2,54 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:native_coins_lib/support_coins.dart';
-export 'support_coins.dart';
+
 export 'encrypt/aes.dart';
 export 'encrypt/base58.dart';
 export 'mnemonic_util.dart';
+export 'support_coins.dart';
 
 class NativeCoinsLib {
   static const MethodChannel _channel = MethodChannel('native_coins_lib');
 
   ///初始化钱包配置
-  static Future<bool?> initWalletConfig({required bool testNet}) {
+  Future<bool?> initWalletConfig({required bool testNet}) {
     return _channel.invokeMethod<bool>("initWalletConfig", {"testNet": testNet});
   }
 
   ///获取所有 单词
-  static Future<List<String>?> getAllMnemonicWords() async {
+  Future<List<String>?> getAllMnemonicWords() async {
     var res = await _channel.invokeMethod<List>("getAllMnemonicWords");
     return res?.map((e) => e.toString()).toList();
   }
 
   ///创建助记词
-  static Future<List<String>?> generateMnemonicWords() async{
-    var res= await _channel.invokeMethod<List>("generateMnemonic");
+  Future<List<String>?> generateMnemonicWords() async {
+    var res = await _channel.invokeMethod<List>("generateMnemonic");
     return res?.map((e) => e.toString()).toList();
   }
 
   ///检查助记词
-  static Future<bool?> checkMnemonicWords(List<String> words) {
+  Future<bool?> checkMnemonicWords(List<String> words) {
     return _channel.invokeMethod<bool>("checkMnemonicWords", {"words": words});
   }
 
   ///地址
-  static Future<String?> getAddress({required List<String> words, required MainCoin coin, String passPhrase = ""}) {
+  Future<String?> getAddress({required List<String> words, required MainCoin coin, String passPhrase = ""}) {
     return _channel.invokeMethod<String>("getAddress", {"words": words, "coinName": coin.coinName, passPhrase: passPhrase});
   }
 
   ///验证地址
-  static Future<bool?> checkAddress(String address, MainCoin coin) {
+  Future<bool?> checkAddress(String address, MainCoin coin) {
     return _channel.invokeMethod<bool>("checkAddress", {"address": address, "coinName": coin.coinName});
   }
 
   ///签名
-  static Future<String?> signTransaction({required String words,
-    required MainCoin coin,
-    required String inputTransaction,
-    required String addressTo,
-    String passPhrase = ""}) {
+  Future<String?> signTransaction(
+      {required String words,
+      required MainCoin coin,
+      required String inputTransaction,
+      required String addressTo,
+      String passPhrase = ""}) {
     return _channel.invokeMethod<String>("signTransaction", {
       "words": words,
       "coinName": coin.coinName,
